5e74103dd1d6de192a88675f284943e1837d19e0
OskrVillVar <alviva379000@gmail.com>
2024-06-03 13:56:11 -0600

    Estructura condicional anidada

---
+++ src/EstructurasCondicionales_Anidadas.kt
@@ -0,0 +1,76 @@
+fun main () {
+    /*
+    //Problema 1
+    print("Ingrese la primer nota:")
+    val nota1: Int = readln().toInt()
+    print("Ingrese la segunda nota:")
+    val nota2: Int = readln().toInt()
+    print("Ingrese la tercero nota:")
+    val nota3: Int = readln().toInt()
+    val promedio = (nota1+nota2+nota3)/3
+
+    if (promedio>=7)
+        print("Promocionado")
+    else
+        if (promedio>=4)
+            print("Regular")
+    else
+        print("libre")
+
+     */
+    /*
+    print("Ingrese la primer nota:")
+    val nota1: Int = readln().toInt()
+    print("Ingrese la segunda nota:")
+    val nota2: Int = readln().toInt()
+    print("Ingrese la tercero nota:")
+    val nota3: Int = readln().toInt()
+    val promedio = (nota1+nota2+nota3)/3
+    val estado = if (promedio>=7) "Promocionado"
+    else if (promedio >= 4)"Regular" else "Reprobado"
+    print ("Estado del alumno $estado")
+    */
+    /*
+    //Problema 2
+    print("Ingrese el primer número:")
+    val n1: Int = readln().toInt()
+    print("Ingrese el segundo número:")
+    val n2: Int = readln().toInt()
+    print("Ingrese el tercer número:")
+    val n3: Int = readln().toInt()
+    var mayor: Int
+    if (n1 > n2) {
+        if (n1 > n3)
+            mayor = n1
+    } else {
+        mayor = n3
+    }
+    if (n2 > n3) {
+        mayor = n2
+    } else {
+        mayor = n3
+    }
+    print("El valor mayor es $mayor")
+    */
+    /*
+    //Problema 3
+    println("Introduce un número entero:")
+    val n = readln().toInt()
+    if (n>0){
+        println("El número es positivo")
+    }else if (n<0){
+        println("El número es negativo")
+    }else{
+        println("El número es nulo")
+    }
+     */
+    //problema 3
+    println("Introduce un número:")
+    val cifras = readln().toInt()
+    if (n>0){
+        println("El número es positivo")
+    }else if (n<0){
+        println("El número es negativo")
+    }else{
+        println("El número es nulo")
+}
