a22d5d4c3207c2fb8816c1d761162b0a3936435e
OskrVillVar <alviva379000@gmail.com>
2024-08-13 10:49:29 -0600

    perdone usted tendria para un cafe

--- src/Conceptos_POO/Proyecto110.kt
+++ src/Conceptos_POO/Proyecto110.kt
@@ -1,2 +1,31 @@
 package Conceptos_POO
 
+class triangulo {
+    var lado1: Int = 0
+    var lado2: Int = 0
+    var lado3: Int = 0
+    fun inicializar() {
+        print("Ingrese lado1: ")
+        lado1 = readln().toInt()
+        print("Ingrese lado2: ")
+        lado2 = readln().toInt()
+        print("Ingrese lado3: ")
+        lado3 = readln().toInt()
+    }
+
+    fun ladomayor() {
+        print("Lado mayor: ")
+        when {
+            lado1 > lado2 && lado1 > lado3 -> println("lado1")
+            lado2 > lado1 && lado2 > lado3 -> println("lado2")
+            else -> println("lado3")
+        }
+    }
+    fun esequilatero(){
+        if(lado1 == lado2 && lado1 == lado3)
+            println("Es un triangulo equilatero")
+        else
+            println("No es un triangulo equilatero")
+
+    }
+}
--- src/Conceptos_POO/Proyecto111.kt
+++ src/Conceptos_POO/Proyecto111.kt
@@ -1,2 +1,33 @@
 package Conceptos_POO
 
+class alumno {
+    var nombre: String = ""
+    var nota: Int = 0
+
+    fun v(){
+        print("Ingrese el nombre del estudiante: ")
+        nombre = readln()
+        print("Ingrese la nota del estudiante: ")
+        nota = readln().toInt()
+    }
+    fun j(){
+        println("Nombre: $nombre y tiene una nota de $nota.")
+    }
+    fun ch(){
+        if(nota>=4)
+            println("regular.")
+        else
+            println("no regular.")
+    }
+}
+fun main(){
+    val est1 = alumno()
+    est1.v()
+    est1.j()
+    est1.ch()
+    println("=================================================")
+    val est2 = alumno()
+    est2.v()
+    est2.j()
+    est2.ch()
+}
