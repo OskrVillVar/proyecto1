4563c4fc5877c85eb45af16a2fffd03bceb2b82c
OskrVillVar <alviva379000@gmail.com>
2024-09-02 13:50:22 -0600

    2/9/24

--- src/colaboracion_de_clases/Proyecto121.kt
+++ src/colaboracion_de_clases/Proyecto121.kt
@@ -1,2 +1,26 @@
 package colaboracion_de_clases
 
+class Socio(val nombre: String, val antiguedad: Int)
+
+
+class Club(val socio1: Socio, private val socio2: Socio, private val socio3: Socio) {
+
+    fun mayorAntiguedad() {
+        val socios = listOf(socio1, socio2, socio3)
+        val mayorAntiguedad = socios.maxByOrNull { it.antiguedad }
+
+        if (mayorAntiguedad != null) {
+            println("El socio con mayor antigüedad es ${mayorAntiguedad.nombre} con ${mayorAntiguedad.antiguedad} años.")
+        } else {
+            println("No hay socios en el club.")
+        }
+    }
+}
+fun main() {
+    val socio1 = Socio("Juan", 5)
+    val socio2 = Socio("Ana", 8)
+    val socio3 = Socio("Pedro", 3)
+
+    val club = Club(socio1, socio2, socio3)
+    club.mayorAntiguedad()
+}
