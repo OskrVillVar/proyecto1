eb1731e2973b29cd2f17c9414c4b3287eca95615
OskrVillVar <alviva379000@gmail.com>
2024-09-03 10:57:30 -0600

    ola ke ase

--- src/colaboracion_de_clases/Proyecto123.kt
+++ src/colaboracion_de_clases/Proyecto123.kt
@@ -1,2 +1,20 @@
 package colaboracion_de_clases
 
+class dado{
+    private var valor: Int = 1
+    fun tirar() {
+        valor = ((Math.random() * 6) + 1).toInt()
+    }
+    fun imprimir() {
+        separador()
+        println("Valor del dado: $valor")
+        separador()
+    }
+    private fun separador() =
+        println("**************************************")
+}
+fun main(parametro: Array<String>) {
+    val dado1 = dado()
+    dado1.tirar()
+    dado1.imprimir()
+}
--- src/colaboracion_de_clases/Proyecto124.kt
+++ src/colaboracion_de_clases/Proyecto124.kt
@@ -1,2 +1,43 @@
 package colaboracion_de_clases
 
+class ola {
+    private var valores: IntArray = IntArray(5)
+
+    init {
+        vals()
+    }
+
+    private fun vals() {
+        for (i in valores.indices) {
+            valores[i] = (Math.random() * 11).toInt()
+        }
+    }
+
+    fun mo() {
+        print("Valores del arreglo: ")
+        for (i in valores.indices) {
+            print(valores[i])
+            if (i < valores.size - 1) {
+            }
+        }
+        println()
+
+    }
+
+        fun mayor() {
+            val mayo = valores.maxOrNull()
+            println("El mayor valor del arreglo es: $mayo")
+        }
+
+        fun menor() {
+            val meno = valores.minOrNull()
+            println("El menor valor del arreglo es: $meno")
+        }
+    }
+
+    fun main() {
+        val arreglo = ola()
+        arreglo.mo()
+        arreglo.mayor()
+        arreglo.menor()
+    }
